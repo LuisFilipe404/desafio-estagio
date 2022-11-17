9ce8abcd0cdc290785d46e97903b56cefd15fc0e
Luís Filipe Silva de Vasconcelos <108852835+LuisFilipe404@users.noreply.github.com>
2022-11-17 15:37:30 -0300

    Update Footer.jsx

--- client/app/src/Layout/Footer.jsx
+++ client/app/src/Layout/Footer.jsx
@@ -5,7 +5,7 @@ import styles from './Footer.module.css'; // Importação do Css
 function Footer () {
     return (
         <footer className={styles.container}>
-            <p>UNIFAVIP - 2022</p>
+            <p>Softmaker - 2022</p>
         </footer>
     )
 }
